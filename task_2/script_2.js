145e6ee0430fcf119efa6dda46c38f1a4974ec65
Vlad Miroshin <ppvlad@mail.ru>
2024-10-24 23:14:22 +0300

    feat (task_2) create realization

--- task_2/script_2.js
+++ task_2/script_2.js
@@ -1,29 +1,15 @@
-/*
+document.addEventListener('DOMContentLoaded', ()=> {
+    function onclick(selector, handler) {
+        document.querySelector(selector).onclick = handler;
+    }
+  
+    onclick('.sample__button', () => toggle_icon());
+});
 
-Задание 2
+const icons = document.querySelectorAll('.sample__icon');
 
-Дан образец JSON-строки.
-Задача — создать JS-объект, который при преобразовании в JSON будет возвращать в качестве результата такую же JSON-строку, как в образце. 
-Получившуюся строку вывести в консоль.
-
-{"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}
-
-*/
-
-const sample = '{"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}';
-
-// Решение
-
-const obj = {
-    name: "Anton",
-    age: 36,
-    skills: ["Javascript", "HTML", "CSS"],
-    salary: 80_000
-};
-
-// проверка
-
-const jsonText = JSON.stringify(obj);
-
-console.log(jsonText);
-console.log(jsonText === sample ? 'Удовлетворяет требованиям' : 'fail');
+function toggle_icon() {
+    icons.forEach(
+        (element) => element.classList.toggle('sample__icon--hide')
+    )
+}
