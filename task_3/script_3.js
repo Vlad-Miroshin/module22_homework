880dccb98c5b989f15fd7a922289a5c37a531f18
Vlad Miroshin <ppvlad@mail.ru>
2024-10-28 19:00:10 +0300

    feat (task_4) create realization

--- task_3/script_3.js
+++ task_3/script_3.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
 });
 
 
+const waiting_message = document.querySelector('.waiting__message');
 const response_container = document.querySelector('#response_container');
 const ul = document.querySelector('.response__list');
 
@@ -18,17 +19,20 @@ function act_fetch() {
     addItem(`Размер экрана - ширина: ${window.screen.width} px, высота: ${window.screen.height} px`);
 
     if ("geolocation" in navigator && navigator.geolocation) {
-        addItem('Браузер поддерживает определение местоположения');
+        showWaiting('Определение местоположения...');
 
         navigator.geolocation.getCurrentPosition(
             (position) => {
+                hideWaiting();
                 const { coords } = position;
                 addItem(`Местоположение - широта: ${coords.latitude} °, долгота: ${coords.longitude} °`);
             },
             () => {
+                hideWaiting();
                 addItem('Информация о местоположении недоступна');
             }
         );        
+
 
       } else {
         addItem('Браузер не поддерживает определение местоположения');
@@ -49,6 +53,13 @@ function addItem(text) {
     ul.appendChild(li);
 }
 
+function removeLastItem() {
+    if (ul.firstChild) {
+        ul.removeChild(ul.lastChild);
+    };
+}
+
+
 function hideResponse() {
     response_container.classList.add('page__content--hidden');
 }
@@ -57,39 +68,11 @@ function showResponse() {
     response_container.classList.remove('page__content--hidden');
 }
 
-function updateResponse(data) {
-    hideResponse();
-
-    clearResponse();
-
-    data.forEach(element => {
-        addItem(element);
-    });
-
-    showResponse();
+function showWaiting(message) {
+    waiting_message.innerHTML = message;
+    waiting_message.classList.remove('waiting__message--hidden');
 }
 
-function getData() {
-    let d = [];
-
-    d.push(`Размер экрана - ширина: ${window.screen.width}px, высота: ${window.screen.height}px`);
-
-    if ("geolocation" in navigator && navigator.geolocation) {
-        d.push('Браузер поддерживает определение местоположения');
-
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                const { coords } = position;
-                d.push(`Местоположение - широта: ${coords.latitude}, долгота: ${coords.longitude}`);
-            },
-            () => {
-                d.push('Информация о местоположении недоступна');
-            }
-        );        
-
-      } else {
-        d.push('Браузер не поддерживает определение местоположения');
-    }    
-
-    return d;
+function hideWaiting() {
+    waiting_message.classList.add('waiting__message--hidden');
 }
--- task_4/script_4.js
+++ task_4/script_4.js
@@ -1,45 +1,85 @@
-/*
-
-Задание 4
-
-Создать Promise, в котором c задержкой в три секунды сгенерировать случайное целое число от 1 до 100. Для создания задержки использовать setTimeout. Если сгенерированное число четное — Promise выполнится успешно (resolve), если нечетное — выполнится с ошибкой (reject). После разрешения Promise обработать результат его выполнения и вывести сообщение в консоль:
-«Завершено успешно. Сгенерированное число — number», если Promise завершился успешно. Вместо number подставить сгенерированное число
-«Завершено с ошибкой. Сгенерированное число — number», если Promise завершился с ошибкой. Вместо number подставить сгенерированное число
-
-*/
-
-const DELAY_MILLISECONDS = 3000;
-
-function getRandomInt(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-}
-
-function isEven(value) {
-    return value % 2 === 0;
-}
-
-console.log(`Ожидание выполнения promise (${DELAY_MILLISECONDS} мсек)...`);
-
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        const number = getRandomInt(1, 100);
-
-        if (isEven(number)) {
-            resolve(`Завершено успешно. Сгенерированное число — ${number}`);
-        } else {
-            reject(`Завершено с ошибкой. Сгенерированное число — ${number}`);
-        }
-    }, DELAY_MILLISECONDS);
-  });
+document.addEventListener('DOMContentLoaded', ()=> {
+    function onclick(selector, handler) {
+        document.querySelector(selector).onclick = handler;
+    }
   
-  promise.then((value) => {
+    onclick('.sample__button', () => act_fetch());
+});
 
-    console.log(value);
 
-  }).catch((value) => {
+const waiting_message = document.querySelector('.waiting__message');
+const response_container = document.querySelector('#response_container');
+const ul = document.querySelector('.response__list');
 
-    console.log(value);
+function act_fetch() {
+    hideResponse();
 
-  });
+    clearResponse();
 
+    if ("geolocation" in navigator && navigator.geolocation) {
+        showWaiting(`Определение местоположения...`);
+
+        navigator.geolocation.getCurrentPosition(geo_success, geo_error);
+
+      } else {
+        addItem('Браузер не поддерживает определение местоположения');
+    }    
+
+    showResponse();
+}
+
+function geo_success(position) {
+    const { coords } = position;
+
+    const url = `https://api.ipgeolocation.io/timezone?apiKey=32bcd4a6e4b548968e7afcdb682ac679&lat=${coords.latitude}&long=${coords.longitude}`;
+
+    showWaiting(`Запрос к TimezoneAPI...`);
+
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            hideWaiting();
+
+            addItem(`Временная зона: ${data.timezone}`);
+            addItem(`Местная дата и время: ${data.date_time_txt}`);
+        })
+        .catch((err) => {
+            hideWaiting();
+        });
+}
+
+function geo_error(position) {
+    addItem('Информация о местоположении недоступна');
+}
+
+function clearResponse() {
+    while (ul.firstChild) {
+        ul.removeChild(ul.lastChild);
+    };
+}
+
+function addItem(text) {
+    const li = document.createElement("li");
+    li.innerText = text;
+    ul.appendChild(li);
+}
+
+function hideResponse() {
+    response_container.classList.add('page__content--hidden');
+}
+
+function showResponse() {
+    response_container.classList.remove('page__content--hidden');
+}
+
+function showWaiting(message) {
+    waiting_message.innerHTML = message;
+    waiting_message.classList.remove('waiting__message--hidden');
+}
+
+function hideWaiting() {
+    waiting_message.classList.add('waiting__message--hidden');
+}
 
