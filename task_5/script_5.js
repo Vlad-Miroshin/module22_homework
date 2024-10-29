29f3b90bf77841c9b7bc831ddd12391cccebe56f
Vlad Miroshin <ppvlad@mail.ru>
2024-10-29 22:29:13 +0300

    feat (task_5) create realization

--- task_5/script_5.js
+++ task_5/script_5.js
@@ -1,83 +1,154 @@
-/*
-
-Задание 5
-
-Написать код приложения, интерфейс которого состоит из поля ввода и кнопки «Получить список задач». При нажатии на кнопку нужно отправить запрос с помощью fetch на URL https://jsonplaceholder.typicode.com/users/3/todos. 
-
-Число 3 представляет собой id пользователя, вместо него нужно подставить число, введенное в поле. Если пользователь с таким id существует, вернется список задач для этого пользователя, каждая задача представлена объектом<
-
-Где title — описание задачи, а completed — флаг, отображающий, выполнена задача или нет. Вывести данный список на страницу, оформив соответствующим образом: в виде списка (ul или ol), выполненные задачи должны быть написаны зачеркнутым текстом. Если пользователь с введенным id не существует, вывести сообщение: «Пользователь с указанным id не найден».
-
-*/
-
 document.addEventListener('DOMContentLoaded', ()=> {
     function onclick(selector, handler) {
         document.querySelector(selector).onclick = handler;
     }
   
-    onclick('#btn_fetch', (event) => act_fetch(event));
+    onclick('#btn_send_message', () => act_send_message());
+    onclick('#btn_send_geo', () => act_send_geo());
+
+    clearChat();
+});
+
+const wsUri = "wss://echo-ws-service.herokuapp.com/";
+
+let socket = null;
+let geoSended = false;
+
+const chat_content = document.querySelector('.chat__content');
+const chat_input = document.querySelector('.chat__input');
+
+chat_input.addEventListener('keyup', (e)=> {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+        act_send_message();
+    }
 });
 
 
-const response_container = document.querySelector('#response_container');
-const ul = document.querySelector('.response__list');
+async function act_send_message() {
+    const text = chat_input.value ? chat_input.value : "Пример сообщения";
+    
+    await ensureSocket();
 
-function act_fetch(event) {
-    event.preventDefault();
-
-    let id = document.querySelector("#user_id").value;
-    if (!id) {
-        id = '1'; // default user id
-    }
-
-    const url = `https://jsonplaceholder.typicode.com/users/${id}/todos`;
-
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            updateResponse(data, id);
-        });
+    socket.send(text);
+    addSended(text);
 }
 
-function clearResponse() {
-    while (ul.firstChild) {
-        ul.removeChild(ul.lastChild);
+async function act_send_geo() {
+    await ensureSocket();
+
+    if ("geolocation" in navigator && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                geoSended = true;
+                socket.send([position.coords.latitude, position.coords.longitude]);
+                addGeolocation(position);
+            },
+            () => {
+                addInfo('Информация о местоположении недоступна');
+            }
+        );        
+    } else {
+        addInfo('Браузер не поддерживает определение местоположения');
+    }    
+}
+
+async function ensureSocket() {
+    if (!socket) {
+        socket = new WebSocket(wsUri);
+
+        let connection_resolvers = [];
+        let checkConnection = () => {
+            return new Promise((resolve, reject) => {
+                if (socket.readyState === WebSocket.OPEN) {
+                    resolve();
+                }
+                else {
+                    connection_resolvers.push({resolve, reject});
+                }
+            });
+        }
+
+        socket.onopen = () => { 
+            connection_resolvers.forEach(r => r.resolve());
+            addInfo(`connected to ${socket.url}`); 
+        }
+        socket.onclose = () => { 
+            addInfo('disconnected'); 
+            socket = null;
+        }
+        socket.onmessage = (evt) => { addReceived(evt.data); }
+        socket.onerror = (evt) => { addInfo(evt.data); }
+
+        await checkConnection();
+    }
+}
+
+function removeChilds(element) {
+    while (element.firstChild) {
+        element.removeChild(element.lastChild);
     };
 }
 
-function addItem(text, className = '') {
-    const li = document.createElement("li");
-    li.innerText = text;
+function clearChat() {
+    removeChilds(chat_content);
+}
 
-    if (className) {
-        li.classList.add(className);
+function addItem(text, classNames = []) {
+    const div = document.createElement("div");
+    div.textContent = text;
+
+    classNames.forEach(element => {
+        div.classList.add(element);
+    })
+
+    chat_content.prepend(div);
+
+    showChat();
+}
+
+function addLink(url, text, classNames = []) {
+    const a = document.createElement("a");
+    a.text = text;
+    a.href = url;
+    a.target = '_blank';
+
+    classNames.forEach(element => {
+        a.classList.add(element);
+    })
+
+    chat_content.prepend(a);
+
+    showChat();
+}
+
+function hideChat() {
+    chat_content.classList.add('chat__content--hidden');
+}
+
+function showChat() {
+    chat_content.classList.remove('chat__content--hidden');
+}
+
+function addInfo(text) {
+    addItem(text, ['chat__info']);
+}
+
+function addSended(text) {
+    addItem(text, ['chat__message', 'chat__message--sended']);
+}
+
+function addGeolocation(position) {
+    const { coords } = position;
+    const text = `Широта: ${coords.latitude} °, долгота: ${coords.longitude} °`;
+    const url = `https://www.openstreetmap.org?mlat=${coords.latitude}&mlon=${coords.longitude}`;
+
+    addLink(url, text, ['chat__message', 'chat__message--geo', 'chat__link']);
+}
+
+function addReceived(text) {
+    if (!geoSended) {
+        addItem(text, ['chat__message', 'chat__message--received']);
     }
 
-    ul.appendChild(li);
-}
-
-function hideResponse() {
-    response_container.classList.add('page__content--hidden');
-}
-
-function showResponse() {
-    response_container.classList.remove('page__content--hidden');
-}
-
-function updateResponse(data, id) {
-    hideResponse();
-
-    clearResponse();
-
-    if (!data || data.length === 0) {
-        addItem(`Пользователь с id=${id} не найден `);
-    } else {
-        data.forEach(element => {
-            addItem(element.title, element.completed ? 'response__list--completed' : '');
-        });
-    }
-
-    showResponse();
+    geoSended = false;
 }
